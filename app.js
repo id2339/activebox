3410f83028e2086d862f0f6f0287ee8daf099541
ilya Drozdov <63677014+id2339@users.noreply.github.com>
2022-12-14 23:34:45 +0300

    Add files of Activebox website layout.

---
+++ app.js
@@ -0,0 +1,64 @@
+$( function(){
+
+// fixed header
+	let header = $("#header");
+	let intro = $("#intro");
+	let introH  = intro.innerHeight();
+	let scrollPos = $(window).scrollTop();
+	let nav = $("#nav");
+	let navToggle = $("#navToggle");
+
+	checkScroll(scrollPos, introH);
+
+	$(window).on("scroll resize", function() {
+	let introH = intro.innerHeight();
+		scrollPos = $(this).scrollTop();
+
+		checkScroll(scrollPos, introH);
+	});
+
+	function checkScroll(scrollPos, introH) {
+		if( scrollPos > introH ) {
+			header.addClass( "fixed" );
+		}
+		else {
+			header.removeClass("fixed");
+		}
+	}
+
+
+// smooth scroll
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
+
+		let elementId = $(this).data('scroll');
+		let elementOffset = $(elementId).offset().top;
+
+		nav.removeClass("show");
+
+		console.log(elementOffset);
+
+		$("html, body").animate({
+			scrollTop: elementOffset - 65
+		}, 700);
+	});
+
+// navToggle
+	navToggle.on("click", function(event) {
+		event.preventDefault();
+		nav.toggleClass("show");
+	});
+
+/* testimonials: https://kenwheeler.github.io/slick/ */
+	let slider = $("#testimonialsSlider ");
+
+	slider.slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		arrows: false,
+		dots: true,
+	});
+});
+
